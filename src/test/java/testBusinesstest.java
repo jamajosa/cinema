65ed9e8544e2efc7cecc52a4f7dba12da8653523
ljhdoiu32hdio <iohdoih32oif>
2020-03-01 22:39:24 +0100

    Added state test

--- src/test/java/testBusinesstest.java
+++ src/test/java/testBusinesstest.java
@@ -1,3 +1,7 @@
+import domain.backlog.BacklogItem;
+import domain.backlog.Doing;
+import domain.backlog.Done;
+import domain.backlog.ToDo;
 import domain.user.MailAdapter;
 import domain.user.NotifyService;
 import domain.user.*;
@@ -7,13 +11,26 @@ import org.junit.jupiter.api.Test;
 class testBusinesstest {
 
 	@Test
-	void DoesTheStatesChange() {
+	void TestDoesNotificationSendingTest() {
 		Userfactory factory = new Userfactory();
 		User user = factory.makeUser("D","Richard");
 		NotifyService n = new MailAdapter();
 		n.setCredentials("email@email.nl");
 		user.addNotifyMedium(n);
 		Assertions.assertEquals(n.notify("Hello"), "Hello email@email.nl");
+	}
+
+	@Test
+	void TestDoesItemStateChangesTest() {
+		BacklogItem backlogitem = new BacklogItem();
+		backlogitem.setCurrentPhase(new ToDo());
+		Assertions.assertEquals(backlogitem.getCurrentPhase() instanceof ToDo, true);
+
+		backlogitem.setCurrentPhase(new Doing());
+		Assertions.assertEquals(backlogitem.getCurrentPhase() instanceof ToDo, false);
+
+		backlogitem.setCurrentPhase(new Done());
+		Assertions.assertEquals(backlogitem.getCurrentPhase() instanceof Done, true);
 	}
 }
 
